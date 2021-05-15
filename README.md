532c4435e5a6e53df3e4ca20f0422333156209b7
LSX-s-Software <2682191498@qq.com>
2021-05-15 16:01:17 +0800

    创建数据库配置文件，更新README，添加.gitattributes

---
+++ README.md
@@ -0,0 +1,126 @@
+# **作业管理系统产品文档**
+
+## Homework Checker
+
+------
+
+## 开发者
+
+[**@NagisaCo** NagisaCo](https://github.com/NagisaCo)
+
+[**@LSX-s-Software** Sixing Lin](https://github.com/LSX-s-Software)
+
+[**@WuSiye** Siye Wu](https://github.com/WuSiye)
+
+------
+
+## 产品介绍
+
+| **产品定位** | 一款服务于大学教师与学生的教学辅助系统                       |
+| ------------ | ------------------------------------------------------------ |
+| **产品描述** | 教师可在此系统上布置作业、批改作业、记录成绩；学生可以通过发送邮件到特定邮箱提交作业；教师批改完毕后会将评语和成绩发送至学生的邮箱。期末时教师可以通过此系统一键导出平时成绩。 |
+| **目标人群** | 大学教师、学生，尤其是程序设计课的教师和学生                 |
+| **使用场景** | 学生提交作业、查看批改结果，教师布置作业、批改作业、记录成绩 |
+
+
+
+## 产品功能结构
+
+### **学生端**（使用QQ）
+
+- 查看教师布置的作业
+- 提交作业
+  - 文本
+  - 附件
+- 查看批改结果
+  - 成绩
+  - 评语
+
+### 教师端
+
+- 查看学生提交的作业
+- 作业打分、写评语
+- 查看、导出平时成绩
+
+- 使用数据库储存学生名单、作业、成绩
+
+
+
+## 数据结构设计
+
+### 类设计
+
+- 学生类（Student）
+  - ID
+  - 学号
+  - 年级、班级
+  - 姓名
+  - 头像URL
+  - 成绩
+- 布置的作业类（Assignment）
+  - ID
+  - 标题
+  - 描述
+  - 截止日期
+  - 目标班级
+- 学生提交的作业类（Homework）
+  - ID
+  - 标题
+  - 内容
+  - 附件URL
+  - 成绩
+  - 评语
+- 班级类
+  - ID
+  - 名称
+  - 上课地点
+  - 上课时间
+  - 人数
+  - 邀请码
+
+### 数据库表设计
+
+- 帐号表（users）
+
+| 字段     | id   | username    | password    | type                       | avatar_url   |
+| -------- | ---- | ----------- | ----------- | -------------------------- | ------------ |
+| 数据类型 | INT  | VARCHAR(50) | VARCHAR(50) | TINYINT(1)                 | VARCHAR(100) |
+| 描述     |      | 用户名      | 密码        | 用户类型（0=教师，1=学生） | 头像URL      |
+
+- 布置的作业表（assignments）
+
+| 字段     | id     | teacher_id   | title       | description | start_date | deadline  | class_id   |
+| -------- | ------ | ------------ | ----------- | ----------- | ---------- | --------- | ---------- |
+| 数据类型 | BIGINT | INT          | VARCHAR(80) | TEXT        | TIMESTAMP  | TIMESTAMP | INT        |
+| 描述     |        | 布置的教师ID | 标题        | 描述        | 布置时间   | 截止时间  | 目标班级ID |
+
+- 提交的作业表（homework）
+
+| 字段     | id     | student_id | content_url | attachment_url | score    | comments |
+| -------- | ------ | ---------- | ----------- | -------------- | -------- | -------- |
+| 数据类型 | BIGINT | INT        | VARCHAR(60) | VARCHAR(60)    | SMALLINT | TEXT     |
+| 描述     |        | 学生ID     | 作答文件URL | 附件URL        | 成绩     | 评语     |
+
+- 班级表（classes）
+
+| 字段     | id     | teacher_id | name        | location    | time        | code    |
+| -------- | ------ | ---------- | ----------- | ----------- | ----------- | ------- |
+| 数据类型 | BIGINT | INT        | VARCHAR(25) | VARCHAR(20) | VARCHAR(10) | CHAR(4) |
+| 描述     |        | 教师ID     | 名称        | 上课地点    | 上课时间    | 邀请码  |
+
+- 学生表（students）
+
+| 字段     | id   | school_num | qq          | class_id | name        | register_time |
+| -------- | ---- | ---------- | ----------- | -------- | ----------- | ------------- |
+| 数据类型 | INT  | CHAR(11)   | VARCHAR(15) | INT      | VARCHAR(20) | TIMESTAMP     |
+| 描述     |      | 学号       | QQ号        | 班级ID   | 姓名        | 注册时间      |
+
+
+
+## 产品界面UI设计
+
+链接：
+
+教师端：https://lanhuapp.com/url/O1nmX
+
+学生端：https://lanhuapp.com/url/Kk0cp
---
+++ Setup.sql
@@ -0,0 +1,96 @@
+-- Database export via SQLPro (https://www.sqlprostudio.com/allapps.html)
+-- Exported by linsixing at 15-05-2021 15:31.
+-- WARNING: This file may contain descructive statements such as DROPs.
+-- Please ensure that you are running the script at the proper location.
+
+
+-- BEGIN TABLE assignments
+DROP TABLE IF EXISTS assignments;
+CREATE TABLE `assignments` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `teacher_id` int NOT NULL,
+  `title` varchar(80) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '',
+  `description` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `start_date` timestamp NOT NULL,
+  `deadline` timestamp NOT NULL,
+  `class_id` int NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Table assignments contains no data. No inserts have been genrated.
+-- Inserting 0 rows into assignments
+
+
+-- END TABLE assignments
+
+-- BEGIN TABLE classes
+DROP TABLE IF EXISTS classes;
+CREATE TABLE `classes` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `teacher_id` int NOT NULL,
+  `name` varchar(25) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '',
+  `location` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `time` varchar(10) DEFAULT NULL,
+  `code` char(4) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Table classes contains no data. No inserts have been genrated.
+-- Inserting 0 rows into classes
+
+
+-- END TABLE classes
+
+-- BEGIN TABLE homework
+DROP TABLE IF EXISTS homework;
+CREATE TABLE `homework` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `student_id` int unsigned NOT NULL,
+  `content_url` varchar(60) NOT NULL,
+  `attachment_url` varchar(60) DEFAULT NULL,
+  `score` smallint NOT NULL DEFAULT '0',
+  `comments` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Table homework contains no data. No inserts have been genrated.
+-- Inserting 0 rows into homework
+
+
+-- END TABLE homework
+
+-- BEGIN TABLE students
+DROP TABLE IF EXISTS students;
+CREATE TABLE `students` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `school_num` char(11) NOT NULL,
+  `qq` varchar(15) NOT NULL,
+  `class_id` int unsigned DEFAULT NULL,
+  `name` varchar(20) NOT NULL,
+  `register_time` timestamp NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Table students contains no data. No inserts have been genrated.
+-- Inserting 0 rows into students
+
+
+-- END TABLE students
+
+-- BEGIN TABLE users
+DROP TABLE IF EXISTS users;
+CREATE TABLE `users` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '',
+  `password` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '',
+  `type` tinyint(1) NOT NULL DEFAULT '0',
+  `avatar_url` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Table users contains no data. No inserts have been genrated.
+-- Inserting 0 rows into users
+
+
+-- END TABLE users
+
