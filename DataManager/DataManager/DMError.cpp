a3ae44a539da43fd2610e406748291cd5bff8fc9
NagisaCo <yjr888@vip.qq.com>
2021-05-20 16:45:13 +0800

    Add DMException

--- DataManager/DataManager/DMError.cpp
+++ DataManager/DataManager/DMError.cpp
@@ -9,43 +9,38 @@
 
 namespace DataManager {
 
-DMError::DMError(DMErrorType error) {
-    this->error = error;
-    switch (error) {
+    DMError::DMError(DMErrorType error) {
+        switch (error) {
         case SUCCESS:
-            message = "Success";
             break;
         case INVALID_EMAIL:
-            message = "Invalid Email";
+            throw DMException::INVALID_EMAIL();
             break;
         case INVALID_PASSWOOD:
-            message = "Invalid Password";
+            throw DMException::INVALID_PASSWOOD();
             break;
         case TARGET_EXISTED:
-            message = "Target Existed";
+            throw DMException::TARGET_EXISTED();
             break;
         case TARGET_NOT_FOUND:
-            message = "Target Not Found";
+            throw DMException::TARGET_NOT_FOUND();
             break;
         case DATABASE_OPERATION_ERROR:
-            message = "Database Operation Error";
-            help = "Refer to console log to get more information.";
+            throw DMException::DATABASE_OPERATION_ERROR();
             break;
         case CONNECTION_ERROR:
-            message = "Database Connection Error";
-            help = "Check network conection and server state.";
+            throw DMException::CONNECTION_ERROR();
             break;
         case INVALID_ARGUMENT:
-            message = "Invalid Argument";
+            throw DMException::INVALID_ARGUMENT();
             break;
         case OBJECT_NOT_INITED:
-            message = "Object Not inited";
-            help = "Attempting to invoke setters before the object is fully inited.";
+            throw DMException::OBJECT_NOT_INITED();
             break;
         default:
-            message = "Unknown Error";
+            throw DMException::UNKOWN_ERROR();
             break;
+        }
     }
-}
 
 }
