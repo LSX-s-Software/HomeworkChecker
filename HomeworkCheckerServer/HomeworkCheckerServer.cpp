1201bcc8f4f26ea02a119471a55df64c9e74df2e
NagisaCo <yjr888@vip.qq.com>
2021-05-25 22:55:53 +0800

    update config hjson

--- HomeworkCheckerServer/HomeworkCheckerServer.cpp
+++ HomeworkCheckerServer/HomeworkCheckerServer.cpp
@@ -9,10 +9,11 @@ int main()
     system("CHCP 65001");
     
     //::ShellExecute(NULL, L"open", L"D:\\Work\\HomeworkChecker\\go-cqhttp\\start.lnk", L"", NULL, SW_SHOWNORMAL);
-    //Sleep(10000);
+    //Sleep(8000);
     try
     {
-        QQMessage::_InitClient("127.0.0.1:6700");
+        //QQMessage::_InitClient("127.0.0.1:6700");
+        QQMessage::_InitClient("114.116.231.126:6700");
         QQMessage::_InitServer(6701);
     }
     catch (std::exception& e)
